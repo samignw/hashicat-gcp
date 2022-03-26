c3b60d6160cfc8f754a68631a3214d43c63ab8ea
Samuel Okediran <sokediran@ignw.io>
2022-03-26 01:29:26 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,15 @@
-// module "network" {
-//     source  = "app.terraform.io/DiranHoldings/network/google"
-//     version = "3.4.0"
+module "network" {
+    source  = "app.terraform.io/DiranHoldings/network/google"
+    version = "3.4.0"
 
-//     project_id   = var.project
-//     network_name = "gaurav-network"
+    project_id   = var.project
+    network_name = "gaurav-network"
 
-//     subnets = [
-//     {
-//         subnet_name   = "gaurav-subnet"
-//         subnet_ip     = "10.100.10.0/24"
-//         subnet_region = var.region
-//     }
-//     ]
-// }
+    subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+    ]
+}
